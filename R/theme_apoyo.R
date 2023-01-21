3e584a6be32c1091d7f9e650dd3c4fef5998a2e1
KeithCaballeroRodriguez <kcaballerounmsm@gmail.com>
2023-01-21 16:17:32 -0500

    agregando funciones de theme

---
+++ R/theme_apoyo.R
@@ -0,0 +1,20 @@
+#' @title Theme de APOYO para gráficos
+#'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+theme_apoyo <- function(fuente="") {
+  theme_light() +
+    theme(
+      text = element_text(size = 14, face = "bold",family = fuente),
+      axis.text = element_text(size = 11,face = "bold",color = "black"),
+      panel.border = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.line = element_line(colour = "black"),
+      axis.ticks = element_line(colour = "black"),
+      plot.title = element_text(hjust = 0.5)
+    )
+}
